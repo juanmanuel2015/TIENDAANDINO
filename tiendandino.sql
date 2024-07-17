a3dab9cb904c3a3e7f0bce684a27e2a28e316783
juan manuel andino <andino_2269@hotmail.com>
2024-07-16 23:06:59 -0300

    tajos para sql

---
+++ tiendandino.sql
@@ -0,0 +1,40 @@
+use TiendaAndino
+SELECT * FROM stg.Ventas
+SELECT * FROM prod.LogMod
+SELECT * FROM.prod.FactTransacciones
+SELECT * FROM.prod.DimClientes
+SELECT * FROM.prod.DimMarcas
+SELECT * FROM.prod.DimMercados
+
+
+SELECT Marca,Producto
+FROM stg.Ventas;
+
+--ORDER BY
+SELECT * FROM stg.Ventas
+ORDER BY [ID Producto] ASC;
+
+
+SELECT * FROM stg.Ventas
+WHERE [Pais Cliente] = 'Estados Unidos';
+
+SELECT * FROM stg.Ventas
+WHERE [Pais Cliente] = 'Argentina';
+
+SELECT * FROM stg.Ventas
+WHERE [Cliente] = 'La Casa Del Repostero';
+
+SELECT * FROM stg.Ventas;
+SELECT MAX(Cantidad) FROM stg.Ventas;
+SELECT MIN(Venta) FROM stg.Ventas;
+
+SELECT * FROM stg.Ventas;
+SELECT * FROM stg.Ventas WHERE Marca = 'Veluz' AND [Centro Logistico] = 'Lawrence';
+
+SELECT * FROM stg.Ventas WHERE [Nombre Vendedor] IN ('Kaley', 'Paula', 'Glen')
+SELECT * FROM stg.Ventas WHERE [Nombre Vendedor] IN ('Kaley', 'Paula', 'Glen')
+SELECT Producto,Cantidad,Venta,Costo
+FROM stg.Ventas;
+
+
+
